c7ecb449782c85e1d391291c8d1665b6d393575d
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-04-23 10:50:33 +0200

    Add import

--- redux-demo/src/index.js
+++ redux-demo/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { addComent } from './actions.js';
+import reducer from './reducer.js'
 
 const store = createStore(reducer);
 
